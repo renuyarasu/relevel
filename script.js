3a67d9c5af1a6cb47953fba946e51653e2b599b7
Renuka Prasad <renu.yarasu@gmail.com>
2022-10-11 21:50:15 +0530

    Find Prime Numbers

--- script.js
+++ script.js
@@ -3,31 +3,47 @@ console.clear();
 
 //  Basic Constructs  Basic Problem Solving - Part 01
 
-/* Given a number N in base 10, write a program to convert it into its binary form. 
+/* Given a number N find a pair of prime numbers whose sum is equal to N. 
 Example:  
-Input N=17;
-Output: 10001  
+Input 5
+Output: (2, 3)  
 */
 
+let Sieve = (n, isPrime) => {
+    // initialise all entries of boolean array as true
+    isPrime[0] = isPrime[1] = false;
 
-let convertToBinary = (num) => {
-    let binaryNum = 0;
-    let rem, quotient, i = 1, step = 1;
-
-    while (num != 0) {
-        rem = num % 2;
-        quotient = parseInt(num/2);
-
-        console.log(`Step: ${step} => ${num}/2, Remainder = ${rem}, Quotient = ${quotient}`);
-        num = quotient;
-        step++;
-
-        binaryNum = binaryNum + rem * i;
-        i = i * 10;
-
-        console.log(`Binary Number: ${binaryNum}`);
+    for (let i = 2; i < n; i++) {
+        isPrime[i] = true;
     }
+
+    for (let p = 2; p * p <= n; p++) {
+        if (isPrime[p] == true) {
+            // update all multiples of p
+            for (let i = p * p; i < n; i++) {
+                isPrime[i] = false;
+            }
+        }
+    }
+    return false;
 }
 
-convertToBinary(16);
-convertToBinary(17);
+
+let FindPrimePair = (n) => {
+    // generate all primes using Sieve algorithm
+    let isPrime = new Array(n + 1);
+
+    for (let i = 0; i < n; i++) {
+        isPrime[i] = false;
+    }
+    Sieve(n, isPrime);
+
+    // traversing all numbers to find first pair
+    for (let i = 0; i < n; i++) {
+        if (isPrime[i] && isPrime[n - i]) {
+            console.log(i + " " + (n - i))
+            return;
+        }
+    }
+}
+FindPrimePair(5); // 2 3
